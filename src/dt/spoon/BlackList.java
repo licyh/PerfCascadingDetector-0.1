7805df54553bad5a9c3bcd48a06347f73ae23213
Jiaxin Li <licyh@126.com>
2017-04-26 05:17:19 +0800

    spoon

--- src/dt/spoon/BlackList.java
+++ src/dt/spoon/BlackList.java
@@ -11,7 +11,7 @@ public class BlackList {
 			,"/home/vagrant/spoontest/hadoop-0.23.3-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/lib/partition/InputSampler.java"
 			//,"/home/vagrant/spoontest/hadoop-0.23.3-src/hadoop-mapreduce-project/src/java/org/apache/hadoop/mapred/Child.java"
 			,"/home/vagrant/spoontest/hadoop-0.23.3-src/hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/record/compiler/ant/RccTask.java"
-			
+			,"/home/vagrant/spoontest/hadoop-0.23.3-src/hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/UTF8ByteArrayUtils.java"
 		);
 	
 	static boolean isBlack(String absoluteFilename) {
