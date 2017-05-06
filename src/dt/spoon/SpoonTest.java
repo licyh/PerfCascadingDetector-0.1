d7d6d14c0f77fbb09d09c834d25f0d920f19a956
Jiaxin Li <licyh@126.com>
2017-05-06 08:51:24 +0800

    ww

--- src/dt/spoon/SpoonTest.java
+++ src/dt/spoon/SpoonTest.java
@@ -15,8 +15,8 @@ public class SpoonTest {
 	public static void main(String[] args) throws Exception {
 		
 		System.out.println("JX - INFO - Under Spoon Testing !!!");
-		//SpoonUtil.guiSpoon( "src/dt/spoon/test/JXTest.java" );
-		SpoonUtil.spoon( "src/dt/spoon/test", ".", "output/" );
+		SpoonUtil.guiSpoon( "src/dt/spoon/test/JXTest.java" );
+		//SpoonUtil.spoon( "src/dt/spoon/test", ".", "output/" );
 		System.out.println("JX - INFO - finished !!!");
 	}
 
