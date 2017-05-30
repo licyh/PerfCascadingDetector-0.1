192a529ab8ab9fce7aae69dfcbc98be9486e94a5
jx <licyh@126.com>
2017-05-30 06:49:33 +0000

    xx

--- src/dm/res/ha-4584.sh
+++ src/dm/res/ha-4584.sh
@@ -8,6 +8,18 @@ dm_dir=/tmp/${bug_id}_dm    #like "/tmp/mr-4576_dm"
 
 
 # Stop hadoop so that NOT under logging
+echo "JX - INFO - clean up HDFS data (need ensure started)"
+start-dfs.sh
+sleep 5s
+hadoop dfsadmin -safemode leave
+sleep 2s
+hadoop fs -rmr input
+sleep 1s
+hadoop fs -mkdir /user/vagrant/input
+sleep 1s
+hadoop fs -lsr /
+sleep 5s
+
 echo "JX - INFO - stop hadoop .."
 stop-dfs.sh
 if [ $? -ne 0 ]; then
@@ -15,6 +27,7 @@ if [ $? -ne 0 ]; then
   exit
 fi
 sleep 3s
+rm ~/hadoop-0.20.205.0/logs/*
 
 # Clean
 echo "JX - INFO - clean $dm_dir .."
