8d483cd875995c6b55533d257e392a20e6c47386
Jiaxin Li <licyh@126.com>
2017-06-16 09:42:45 +0800

    x

---
+++ src/sa/loopsize/StaticAnalysis.java
@@ -0,0 +1,17 @@
+package sa.loopsize;
+
+import sa.wala.WalaAnalyzer;
+
+public class StaticAnalysis {
+	
+	WalaAnalyzer walaAnalyzer;
+	
+	public StaticAnalysis() {
+		this.walaAnalyzer = new WalaAnalyzer("src/sa/res/ha-4584");
+	}
+	
+	
+}
+
+
+
