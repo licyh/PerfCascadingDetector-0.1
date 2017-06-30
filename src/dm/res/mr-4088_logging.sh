958a8d6eb1c4670c5bc952eee05ddf9b9647ce7f
jx LI <licyh@126.com>
2017-06-30 17:25:17 +0000

    x

--- src/dm/res/mr-4088_logging.sh
+++ src/dm/res/mr-4088_logging.sh
@@ -10,8 +10,8 @@ sleep 2s
 nohup hadoop jar ~/hadoop/install/hadoop-0.20.205.0/hadoop-examples-0.20.205.0.jar wordcount input/1K.file output/1K.file &
 hadoop jar ~/hadoop/install/hadoop-0.20.205.0/hadoop-examples-0.20.205.0.jar wordcount input/10K.file output/10K.file
 
-echo "waiting for 2s to end"
-sleep 2s
+echo "waiting for 5s to end"
+sleep 5s
 
 #stop-dfs.sh
 hadoop-daemon.sh stop tasktracker
