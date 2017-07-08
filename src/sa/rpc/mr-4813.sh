6a8b7a05234e6aeb69c0dd59c4ff35ef45f50070
JX <licyh@126.com>
2017-07-08 23:20:30 +0000

    xx

---
+++ src/sa/rpc/mr-4813.sh
@@ -0,0 +1,11 @@
+# Please set the project home directory
+project_dir=/home/vagrant/JXCascading-detector             #JX - NO "/" at the end
+
+bug_id=mr-4813
+jars_dir=$project_dir/src/sa/res/mr-4813
+
+
+# Call real RPC Finder
+echo "JX - INFO - call rpcfinder.sh .."
+cd $project_dir/src/sa/rpc
+./rpcfinder.sh $jars_dir 
