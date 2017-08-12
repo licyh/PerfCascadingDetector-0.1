1742b7363acc3fd9ba3621bf74af3df03c9f1c27
Jiaxin Li <licyh@126.com>
2017-08-12 16:05:11 +0800

    x

--- src/ds/loganalysis.py
+++ src/ds/loganalysis.py
@@ -3,18 +3,20 @@
 filename_input = '/tmp/dynamicpoints.txt'
 filename_output = '/tmp/dynamicpoints.out'
 
-dict = {}
+set = set()
 
 with open(filename_input, 'r') as fin:
     for line in fin.readlines():
         line = line.strip()
         str = line.split(' ', 3)[-1];
-        print(str)
+        #print(str)
+        set.add(str)
         
+
 '''
 sortedlist = sorted(dict.items(), key=lambda x:x[1], reverse=True)
-
-with open(filename_output, 'w') as fout:
-    for tup in sortedlist:
-        fout.write("(%s, %d)\n" % (tup[0], tup[1]))
 '''
+        
+with open(filename_output, 'w') as fout:
+    for str in set:
+        fout.write("%s\n" % str)
