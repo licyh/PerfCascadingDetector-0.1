a5ce961169559a7e249ddb7825b821be18880cf9
Jiaxin Li <licyh@126.com>
2017-08-18 02:44:07 +0800

    x

--- src/da/cascading/LockCase.java
+++ src/da/cascading/LockCase.java
@@ -291,9 +291,9 @@ public class LockCase {
     	// for needed in the future
         for (Pair pair: hbg.getBackEdge().get(x)) {
         	int y = pair.destination;
-        	//if ( !traversedNodes.get(y) && hbg.getReachSet().get(y).get(endIndex) ) {
+        	if ( !traversedNodes.get(y) && hbg.getReachSet().get(y).get(endIndex) ) {
         		dfsTraversing2( y, -1, endIndex, resources );
-        	//}
+        	}
         }
     }
     
