a833daedcfc4de93103a60b94b521f73ca49140c
jx <licyh@126.com>
2017-08-18 07:43:19 +0000

    x

---
+++ src/dm/res/ds-ha-4584_pre.sh
@@ -0,0 +1,50 @@
+# Please set the project home directory        
+project_dir=/home/vagrant/JXCascading-detector          #JX - NO "/" at the end
+
+
+bug_id=ha-4584
+dm_dir=/tmp/${bug_id}_dm    #like "/tmp/mr-4576_dm"
+
+
+
+# Start hadoop to clean up data
+echo "JX - INFO - clean up HDFS data (need ensure started)"
+start-dfs.sh
+sleep 5s
+hadoop dfsadmin -safemode leave
+sleep 2s
+hadoop fs -rmr input
+sleep 1s
+hadoop fs -mkdir /user/vagrant/input
+sleep 1s
+hadoop fs -lsr /
+sleep 5s
+
+# Stop hadoop so that NOT under logging
+echo "JX - INFO - stop hadoop .."
+stop-dfs.sh
+if [ $? -ne 0 ]; then
+  echo "stop-all.sh error"
+  exit
+fi
+sleep 3s
+rm ~/hadoop-0.20.205.0/logs/*
+
+# Clean
+echo "JX - INFO - clean $dm_dir .."
+if [ -d $dm_dir ]; then
+  rm -rf $dm_dir
+fi
+mkdir $dm_dir
+
+
+# Compile
+echo "JX - INFO - compile jar-dm-$bug_id .."
+cd $project_dir
+ant jar-ds-$bug_id
+if [ $? -ne 0 ]; then
+  echo "compile error"
+  exit
+fi
+
+
---
+++ src/ds/res/ha-4584.sh
@@ -0,0 +1,12 @@
+project_dir=/home/vagrant/JXCascading-detector          #JX - NO "/" at the end
+
+bug_id=ha-4584
+
+
+cd /tmp/${bug_id}_dm
+grep -ri DynamicPoint > $project_dir/src/ds/res/$bug_id/dynamicpoints.txt
+
+cd $project_dir
+cd src/ds/res
+python dataAnalysis.py $project_dir/src/ds/res/$bug_id/dynamicpoints.txt $project_dir/src/ds/res/$bug_id/dynamicpoints.out
+
