da9b60a729808e749f3285e72877e218c7628c65
xin jia li <licyh@126.com>
2017-08-22 02:19:48 +0000

    x

--- src/dm/res/ca-6744_logging_begin_sh1_node2.sh
+++ src/dm/res/ca-6744_logging_begin_sh1_node2.sh
@@ -1,3 +1,4 @@
 
+sleep 15s
 jps
 cassandra -f
--- src/dm/res/ca-6744_logging_end_sh2_node2.sh
+++ src/dm/res/ca-6744_logging_end_sh2_node2.sh
@@ -1,12 +1,20 @@
 
-nodetool ring
-sleep 1s
+#add in sh1
+sleep 15s
 
-nodetool clean
+
+echo "JX - INFO - waiting of cassandra node2 to start .."
+sleep 40s
+
+nodetool ring
+sleep 2s
+
+echo "JX - INFO - do cleanup & flush on cassandra node 2.."
+nodetool cleanup
 sleep 1s
 
 nodetool flush
-sleep 3s
+sleep 5s
 
 # Stop cassandra node2
 echo "JX - INFO - stop cassandra node 2.."
