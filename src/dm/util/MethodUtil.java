c889ab2ef6952f0e4c506d028f0fa07d231b3982
Jiaxin Li <licyh@126.com>
2017-08-26 10:28:16 +0800

    x

--- src/dm/util/MethodUtil.java
+++ src/dm/util/MethodUtil.java
@@ -242,11 +242,11 @@ public class MethodUtil {
 					}
 			        else if ( apiClass.equals("java.util.List") && apiMethod.equals("remove") ) {
 						m.replace( "{"
+								+ "$_ = $proceed($$);"
 								+ "String opValue = System.identityHashCode($_);"
-								+ logClass + "." + logMethod + "( opValue );" 
-								+ "$_ = $proceed($$);" 
+								+ logClass + "." + "log_EventProcExit" + "( opValue );" 
 								+ "String opValue_2 = System.identityHashCode($_);"
-								+ logClass + "." + "log_EventProcExit" + "( opValue_2 );"
+								+ logClass + "." + logMethod + "( opValue_2 );"
 								+ "}" );
 					}
 			        else {
