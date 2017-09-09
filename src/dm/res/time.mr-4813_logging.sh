f475e743f46e4c6858567f99875645d76f9371bd
JX <licyh@126.com>
2017-09-09 07:06:51 +0000

    x

---
+++ src/dm/res/time.mr-4813_logging.sh
@@ -0,0 +1,318 @@
+17/09/09 06:47:56 INFO ipc.Client: Retrying connect to server: 0.0.0.0/0.0.0.0:8032. Already tried 0 time(s).
+17/09/09 06:47:57 INFO ipc.Client: Retrying connect to server: 0.0.0.0/0.0.0.0:8032. Already tried 1 time(s).
+17/09/09 06:47:58 INFO ipc.Client: Retrying connect to server: 0.0.0.0/0.0.0.0:8032. Already tried 2 time(s).
+17/09/09 06:48:08 INFO input.FileInputFormat: Total input paths to process : 1
+17/09/09 06:48:08 INFO util.NativeCodeLoader: Loaded the native-hadoop library
+17/09/09 06:48:08 WARN snappy.LoadSnappy: Snappy native library not loaded
+17/09/09 06:48:08 INFO mapreduce.JobSubmitter: number of splits:1
+17/09/09 06:48:08 WARN conf.Configuration: mapreduce.combine.class is deprecated. Instead, use mapreduce.job.combine.class
+17/09/09 06:48:08 WARN conf.Configuration: mapred.used.genericoptionsparser is deprecated. Instead, use mapreduce.client.genericoptionsparser.used
+17/09/09 06:48:08 WARN conf.Configuration: mapred.jar is deprecated. Instead, use mapreduce.job.jar
+17/09/09 06:48:08 WARN conf.Configuration: mapreduce.map.class is deprecated. Instead, use mapreduce.job.map.class
+17/09/09 06:48:08 WARN conf.Configuration: mapred.job.name is deprecated. Instead, use mapreduce.job.name
+17/09/09 06:48:08 WARN conf.Configuration: mapreduce.reduce.class is deprecated. Instead, use mapreduce.job.reduce.class
+17/09/09 06:48:08 WARN conf.Configuration: mapred.input.dir is deprecated. Instead, use mapreduce.input.fileinputformat.inputdir
+17/09/09 06:48:08 WARN conf.Configuration: mapred.output.dir is deprecated. Instead, use mapreduce.output.fileoutputformat.outputdir
+17/09/09 06:48:08 WARN conf.Configuration: mapred.map.tasks is deprecated. Instead, use mapreduce.job.maps
+17/09/09 06:48:08 WARN conf.Configuration: mapred.reduce.tasks is deprecated. Instead, use mapreduce.job.reduces
+17/09/09 06:48:08 WARN conf.Configuration: mapred.output.value.class is deprecated. Instead, use mapreduce.job.output.value.class
+17/09/09 06:48:08 WARN conf.Configuration: mapred.output.key.class is deprecated. Instead, use mapreduce.job.output.key.class
+17/09/09 06:48:08 WARN conf.Configuration: mapred.working.dir is deprecated. Instead, use mapreduce.job.working.dir
+17/09/09 06:48:09 INFO mapred.ResourceMgrDelegate: Submitted application application_1504939666291_0001 to ResourceManager at 0.0.0.0/0.0.0.0:8032
+17/09/09 06:48:09 INFO mapreduce.Job: The url to track the job: http://hadoop1:8088/proxy/application_1504939666291_0001/
+17/09/09 06:48:09 INFO mapreduce.Job: Running job: job_1504939666291_0001
+17/09/09 06:48:43 INFO mapreduce.Job: Job job_1504939666291_0001 running in uber mode : false
+17/09/09 06:48:43 INFO mapreduce.Job:  map 0% reduce 0%
+17/09/09 06:48:54 INFO mapreduce.Job:  map 100% reduce 0%
+17/09/09 06:49:09 INFO mapreduce.Job:  map 100% reduce 100%
+17/09/09 06:49:09 INFO mapreduce.Job: Job job_1504939666291_0001 completed successfully
+17/09/09 06:49:09 INFO mapreduce.Job: Counters: 43
+	File System Counters
+		FILE: Number of bytes read=2628
+		FILE: Number of bytes written=123487
+		FILE: Number of read operations=0
+		FILE: Number of large read operations=0
+		FILE: Number of write operations=0
+		HDFS: Number of bytes read=1500
+		HDFS: Number of bytes written=1458
+		HDFS: Number of read operations=6
+		HDFS: Number of large read operations=0
+		HDFS: Number of write operations=2
+	Job Counters 
+		Launched map tasks=1
+		Launched reduce tasks=1
+		Data-local map tasks=1
+		Total time spent by all maps in occupied slots (ms)=88176
+		Total time spent by all reduces in occupied slots (ms)=101192
+	Map-Reduce Framework
+		Map input records=18
+		Map output records=317
+		Map output bytes=2439
+		Map output materialized bytes=2388
+		Input split bytes=114
+		Combine input records=317
+		Combine output records=231
+		Reduce input groups=231
+		Reduce shuffle bytes=2388
+		Reduce input records=231
+		Reduce output records=231
+		Spilled Records=462
+		Shuffled Maps =1
+		Failed Shuffles=0
+		Merged Map outputs=1
+		GC time elapsed (ms)=287
+		CPU time spent (ms)=8850
+		Physical memory (bytes) snapshot=447991808
+		Virtual memory (bytes) snapshot=1664892928
+		Total committed heap usage (bytes)=402128896
+	Shuffle Errors
+		BAD_ID=0
+		CONNECTION=0
+		IO_ERROR=0
+		WRONG_LENGTH=0
+		WRONG_MAP=0
+		WRONG_REDUCE=0
+	File Input Format Counters 
+		Bytes Read=1386
+	File Output Format Counters 
+		Bytes Written=1458
+real 93.13
+user 9.30
+sys 6.59
+17/09/09 06:51:35 INFO ipc.Client: Retrying connect to server: 0.0.0.0/0.0.0.0:8032. Already tried 0 time(s).
+17/09/09 06:51:36 INFO ipc.Client: Retrying connect to server: 0.0.0.0/0.0.0.0:8032. Already tried 1 time(s).
+17/09/09 06:51:37 INFO ipc.Client: Retrying connect to server: 0.0.0.0/0.0.0.0:8032. Already tried 2 time(s).
+17/09/09 06:51:46 INFO input.FileInputFormat: Total input paths to process : 1
+17/09/09 06:51:46 INFO util.NativeCodeLoader: Loaded the native-hadoop library
+17/09/09 06:51:46 WARN snappy.LoadSnappy: Snappy native library not loaded
+17/09/09 06:51:46 INFO mapreduce.JobSubmitter: number of splits:1
+17/09/09 06:51:46 WARN conf.Configuration: mapreduce.combine.class is deprecated. Instead, use mapreduce.job.combine.class
+17/09/09 06:51:46 WARN conf.Configuration: mapred.used.genericoptionsparser is deprecated. Instead, use mapreduce.client.genericoptionsparser.used
+17/09/09 06:51:46 WARN conf.Configuration: mapred.jar is deprecated. Instead, use mapreduce.job.jar
+17/09/09 06:51:46 WARN conf.Configuration: mapreduce.map.class is deprecated. Instead, use mapreduce.job.map.class
+17/09/09 06:51:46 WARN conf.Configuration: mapred.job.name is deprecated. Instead, use mapreduce.job.name
+17/09/09 06:51:46 WARN conf.Configuration: mapreduce.reduce.class is deprecated. Instead, use mapreduce.job.reduce.class
+17/09/09 06:51:46 WARN conf.Configuration: mapred.input.dir is deprecated. Instead, use mapreduce.input.fileinputformat.inputdir
+17/09/09 06:51:46 WARN conf.Configuration: mapred.output.dir is deprecated. Instead, use mapreduce.output.fileoutputformat.outputdir
+17/09/09 06:51:46 WARN conf.Configuration: mapred.map.tasks is deprecated. Instead, use mapreduce.job.maps
+17/09/09 06:51:46 WARN conf.Configuration: mapred.reduce.tasks is deprecated. Instead, use mapreduce.job.reduces
+17/09/09 06:51:46 WARN conf.Configuration: mapred.output.value.class is deprecated. Instead, use mapreduce.job.output.value.class
+17/09/09 06:51:46 WARN conf.Configuration: mapred.output.key.class is deprecated. Instead, use mapreduce.job.output.key.class
+17/09/09 06:51:46 WARN conf.Configuration: mapred.working.dir is deprecated. Instead, use mapreduce.job.working.dir
+17/09/09 06:51:47 INFO mapred.ResourceMgrDelegate: Submitted application application_1504939884684_0001 to ResourceManager at 0.0.0.0/0.0.0.0:8032
+17/09/09 06:51:47 INFO mapreduce.Job: The url to track the job: http://hadoop1:8088/proxy/application_1504939884684_0001/
+17/09/09 06:51:47 INFO mapreduce.Job: Running job: job_1504939884684_0001
+17/09/09 06:52:19 INFO mapreduce.Job: Job job_1504939884684_0001 running in uber mode : false
+17/09/09 06:52:19 INFO mapreduce.Job:  map 0% reduce 0%
+17/09/09 06:52:30 INFO mapreduce.Job:  map 100% reduce 0%
+17/09/09 06:52:45 INFO mapreduce.Job:  map 100% reduce 100%
+17/09/09 06:52:45 INFO mapreduce.Job: Job job_1504939884684_0001 completed successfully
+17/09/09 06:52:45 INFO mapreduce.Job: Counters: 43
+	File System Counters
+		FILE: Number of bytes read=2628
+		FILE: Number of bytes written=123487
+		FILE: Number of read operations=0
+		FILE: Number of large read operations=0
+		FILE: Number of write operations=0
+		HDFS: Number of bytes read=1500
+		HDFS: Number of bytes written=1458
+		HDFS: Number of read operations=6
+		HDFS: Number of large read operations=0
+		HDFS: Number of write operations=2
+	Job Counters 
+		Launched map tasks=1
+		Launched reduce tasks=1
+		Data-local map tasks=1
+		Total time spent by all maps in occupied slots (ms)=84280
+		Total time spent by all reduces in occupied slots (ms)=105080
+	Map-Reduce Framework
+		Map input records=18
+		Map output records=317
+		Map output bytes=2439
+		Map output materialized bytes=2388
+		Input split bytes=114
+		Combine input records=317
+		Combine output records=231
+		Reduce input groups=231
+		Reduce shuffle bytes=2388
+		Reduce input records=231
+		Reduce output records=231
+		Spilled Records=462
+		Shuffled Maps =1
+		Failed Shuffles=0
+		Merged Map outputs=1
+		GC time elapsed (ms)=218
+		CPU time spent (ms)=9420
+		Physical memory (bytes) snapshot=447250432
+		Virtual memory (bytes) snapshot=1664876544
+		Total committed heap usage (bytes)=402128896
+	Shuffle Errors
+		BAD_ID=0
+		CONNECTION=0
+		IO_ERROR=0
+		WRONG_LENGTH=0
+		WRONG_MAP=0
+		WRONG_REDUCE=0
+	File Input Format Counters 
+		Bytes Read=1386
+	File Output Format Counters 
+		Bytes Written=1458
+real 90.83
+user 9.58
+sys 7.38
+17/09/09 06:59:32 INFO input.FileInputFormat: Total input paths to process : 1
+17/09/09 06:59:32 INFO util.NativeCodeLoader: Loaded the native-hadoop library
+17/09/09 06:59:32 WARN snappy.LoadSnappy: Snappy native library not loaded
+17/09/09 06:59:32 INFO mapreduce.JobSubmitter: number of splits:1
+17/09/09 06:59:32 WARN conf.Configuration: mapreduce.combine.class is deprecated. Instead, use mapreduce.job.combine.class
+17/09/09 06:59:32 WARN conf.Configuration: mapred.used.genericoptionsparser is deprecated. Instead, use mapreduce.client.genericoptionsparser.used
+17/09/09 06:59:32 WARN conf.Configuration: mapred.jar is deprecated. Instead, use mapreduce.job.jar
+17/09/09 06:59:32 WARN conf.Configuration: mapreduce.map.class is deprecated. Instead, use mapreduce.job.map.class
+17/09/09 06:59:32 WARN conf.Configuration: mapred.job.name is deprecated. Instead, use mapreduce.job.name
+17/09/09 06:59:32 WARN conf.Configuration: mapreduce.reduce.class is deprecated. Instead, use mapreduce.job.reduce.class
+17/09/09 06:59:32 WARN conf.Configuration: mapred.input.dir is deprecated. Instead, use mapreduce.input.fileinputformat.inputdir
+17/09/09 06:59:32 WARN conf.Configuration: mapred.output.dir is deprecated. Instead, use mapreduce.output.fileoutputformat.outputdir
+17/09/09 06:59:32 WARN conf.Configuration: mapred.map.tasks is deprecated. Instead, use mapreduce.job.maps
+17/09/09 06:59:32 WARN conf.Configuration: mapred.reduce.tasks is deprecated. Instead, use mapreduce.job.reduces
+17/09/09 06:59:32 WARN conf.Configuration: mapred.output.value.class is deprecated. Instead, use mapreduce.job.output.value.class
+17/09/09 06:59:32 WARN conf.Configuration: mapred.output.key.class is deprecated. Instead, use mapreduce.job.output.key.class
+17/09/09 06:59:32 WARN conf.Configuration: mapred.working.dir is deprecated. Instead, use mapreduce.job.working.dir
+17/09/09 06:59:33 INFO mapred.ResourceMgrDelegate: Submitted application application_1504940360507_0001 to ResourceManager at /0.0.0.0:8032
+17/09/09 06:59:33 INFO mapreduce.Job: The url to track the job: http://hadoop1:8088/proxy/application_1504940360507_0001/
+17/09/09 06:59:33 INFO mapreduce.Job: Running job: job_1504940360507_0001
+17/09/09 06:59:59 INFO mapreduce.Job: Job job_1504940360507_0001 running in uber mode : false
+17/09/09 06:59:59 INFO mapreduce.Job:  map 0% reduce 0%
+17/09/09 07:00:12 INFO mapreduce.Job:  map 100% reduce 0%
+17/09/09 07:00:27 INFO mapreduce.Job:  map 100% reduce 100%
+17/09/09 07:00:27 INFO mapreduce.Job: Job job_1504940360507_0001 completed successfully
+17/09/09 07:00:28 INFO mapreduce.Job: Counters: 43
+	File System Counters
+		FILE: Number of bytes read=2628
+		FILE: Number of bytes written=123487
+		FILE: Number of read operations=0
+		FILE: Number of large read operations=0
+		FILE: Number of write operations=0
+		HDFS: Number of bytes read=1500
+		HDFS: Number of bytes written=1458
+		HDFS: Number of read operations=6
+		HDFS: Number of large read operations=0
+		HDFS: Number of write operations=2
+	Job Counters 
+		Launched map tasks=1
+		Launched reduce tasks=1
+		Data-local map tasks=1
+		Total time spent by all maps in occupied slots (ms)=87136
+		Total time spent by all reduces in occupied slots (ms)=100496
+	Map-Reduce Framework
+		Map input records=18
+		Map output records=317
+		Map output bytes=2439
+		Map output materialized bytes=2388
+		Input split bytes=114
+		Combine input records=317
+		Combine output records=231
+		Reduce input groups=231
+		Reduce shuffle bytes=2388
+		Reduce input records=231
+		Reduce output records=231
+		Spilled Records=462
+		Shuffled Maps =1
+		Failed Shuffles=0
+		Merged Map outputs=1
+		GC time elapsed (ms)=187
+		CPU time spent (ms)=8920
+		Physical memory (bytes) snapshot=442630144
+		Virtual memory (bytes) snapshot=1665171456
+		Total committed heap usage (bytes)=401604608
+	Shuffle Errors
+		BAD_ID=0
+		CONNECTION=0
+		IO_ERROR=0
+		WRONG_LENGTH=0
+		WRONG_MAP=0
+		WRONG_REDUCE=0
+	File Input Format Counters 
+		Bytes Read=1386
+	File Output Format Counters 
+		Bytes Written=1458
+real 76.04
+user 8.20
+sys 4.03
+17/09/09 07:03:46 INFO input.FileInputFormat: Total input paths to process : 1
+17/09/09 07:03:46 INFO util.NativeCodeLoader: Loaded the native-hadoop library
+17/09/09 07:03:46 WARN snappy.LoadSnappy: Snappy native library not loaded
+17/09/09 07:03:46 INFO mapreduce.JobSubmitter: number of splits:1
+17/09/09 07:03:46 WARN conf.Configuration: mapreduce.combine.class is deprecated. Instead, use mapreduce.job.combine.class
+17/09/09 07:03:46 WARN conf.Configuration: mapred.used.genericoptionsparser is deprecated. Instead, use mapreduce.client.genericoptionsparser.used
+17/09/09 07:03:46 WARN conf.Configuration: mapred.jar is deprecated. Instead, use mapreduce.job.jar
+17/09/09 07:03:46 WARN conf.Configuration: mapreduce.map.class is deprecated. Instead, use mapreduce.job.map.class
+17/09/09 07:03:46 WARN conf.Configuration: mapred.job.name is deprecated. Instead, use mapreduce.job.name
+17/09/09 07:03:46 WARN conf.Configuration: mapreduce.reduce.class is deprecated. Instead, use mapreduce.job.reduce.class
+17/09/09 07:03:46 WARN conf.Configuration: mapred.input.dir is deprecated. Instead, use mapreduce.input.fileinputformat.inputdir
+17/09/09 07:03:46 WARN conf.Configuration: mapred.output.dir is deprecated. Instead, use mapreduce.output.fileoutputformat.outputdir
+17/09/09 07:03:46 WARN conf.Configuration: mapred.map.tasks is deprecated. Instead, use mapreduce.job.maps
+17/09/09 07:03:46 WARN conf.Configuration: mapred.reduce.tasks is deprecated. Instead, use mapreduce.job.reduces
+17/09/09 07:03:46 WARN conf.Configuration: mapred.output.value.class is deprecated. Instead, use mapreduce.job.output.value.class
+17/09/09 07:03:46 WARN conf.Configuration: mapred.output.key.class is deprecated. Instead, use mapreduce.job.output.key.class
+17/09/09 07:03:46 WARN conf.Configuration: mapred.working.dir is deprecated. Instead, use mapreduce.job.working.dir
+17/09/09 07:03:47 INFO mapred.ResourceMgrDelegate: Submitted application application_1504940615028_0001 to ResourceManager at /0.0.0.0:8032
+17/09/09 07:03:48 INFO mapreduce.Job: The url to track the job: http://hadoop1:8088/proxy/application_1504940615028_0001/
+17/09/09 07:03:48 INFO mapreduce.Job: Running job: job_1504940615028_0001
+17/09/09 07:04:10 INFO mapreduce.Job: Job job_1504940615028_0001 running in uber mode : false
+17/09/09 07:04:10 INFO mapreduce.Job:  map 0% reduce 0%
+17/09/09 07:04:22 INFO mapreduce.Job:  map 100% reduce 0%
+17/09/09 07:04:37 INFO mapreduce.Job:  map 100% reduce 100%
+17/09/09 07:04:37 INFO mapreduce.Job: Job job_1504940615028_0001 completed successfully
+17/09/09 07:04:38 INFO mapreduce.Job: Counters: 43
+	File System Counters
+		FILE: Number of bytes read=2628
+		FILE: Number of bytes written=123487
+		FILE: Number of read operations=0
+		FILE: Number of large read operations=0
+		FILE: Number of write operations=0
+		HDFS: Number of bytes read=1500
+		HDFS: Number of bytes written=1458
+		HDFS: Number of read operations=6
+		HDFS: Number of large read operations=0
+		HDFS: Number of write operations=2
+	Job Counters 
+		Launched map tasks=1
+		Launched reduce tasks=1
+		Data-local map tasks=1
+		Total time spent by all maps in occupied slots (ms)=87864
+		Total time spent by all reduces in occupied slots (ms)=109912
+	Map-Reduce Framework
+		Map input records=18
+		Map output records=317
+		Map output bytes=2439
+		Map output materialized bytes=2388
+		Input split bytes=114
+		Combine input records=317
+		Combine output records=231
+		Reduce input groups=231
+		Reduce shuffle bytes=2388
+		Reduce input records=231
+		Reduce output records=231
+		Spilled Records=462
+		Shuffled Maps =1
+		Failed Shuffles=0
+		Merged Map outputs=1
+		GC time elapsed (ms)=277
+		CPU time spent (ms)=8870
+		Physical memory (bytes) snapshot=451645440
+		Virtual memory (bytes) snapshot=1664733184
+		Total committed heap usage (bytes)=402128896
+	Shuffle Errors
+		BAD_ID=0
+		CONNECTION=0
+		IO_ERROR=0
+		WRONG_LENGTH=0
+		WRONG_MAP=0
+		WRONG_REDUCE=0
+	File Input Format Counters 
+		Bytes Read=1386
+	File Output Format Counters 
+		Bytes Written=1458
+real 70.89
+user 8.53
+sys 5.35
