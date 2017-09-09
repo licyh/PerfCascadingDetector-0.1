0fc268924246e99e51b22a4888fc97041842ce5f
jx <licyh@126.com>
2017-09-09 06:02:43 +0000

    x

---
+++ src/dm/res/time.ha-4584_logging.sh
@@ -0,0 +1,84 @@
+Warning: $HADOOP_HOME is deprecated.
+
+java.lang.NullPointerException
+	at dm.util.MethodUtil.setMethod(Unknown Source)
+	at dm.util.MethodUtil.<init>(Unknown Source)
+	at dm.transformers.Transformers.transformClassForCodeSnippets(Unknown Source)
+	at dm.HDFSTransformer.transformClass(Unknown Source)
+	at dm.Transformer.transformClass(Unknown Source)
+	at dm.Transformer.transform(Unknown Source)
+	at sun.instrument.TransformerManager.transform(TransformerManager.java:188)
+	at sun.instrument.InstrumentationImpl.transform(InstrumentationImpl.java:424)
+	at com.sun.security.auth.module.UnixLoginModule.login(UnixLoginModule.java:124)
+	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
+	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.lang.reflect.Method.invoke(Method.java:606)
+	at javax.security.auth.login.LoginContext.invoke(LoginContext.java:762)
+	at javax.security.auth.login.LoginContext.access$000(LoginContext.java:203)
+	at javax.security.auth.login.LoginContext$4.run(LoginContext.java:690)
+	at javax.security.auth.login.LoginContext$4.run(LoginContext.java:688)
+	at java.security.AccessController.doPrivileged(Native Method)
+	at javax.security.auth.login.LoginContext.invokePriv(LoginContext.java:687)
+	at javax.security.auth.login.LoginContext.login(LoginContext.java:595)
+	at org.apache.hadoop.security.UserGroupInformation.getLoginUser(UserGroupInformation.java:414)
+	at org.apache.hadoop.security.UserGroupInformation.getCurrentUser(UserGroupInformation.java:395)
+	at org.apache.hadoop.fs.FileSystem$Cache$Key.<init>(FileSystem.java:1436)
+	at org.apache.hadoop.fs.FileSystem$Cache.get(FileSystem.java:1337)
+	at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:244)
+	at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:122)
+	at org.apache.hadoop.fs.FsShell.init(FsShell.java:82)
+	at org.apache.hadoop.fs.FsShell.run(FsShell.java:1745)
+	at org.apache.hadoop.util.ToolRunner.run(ToolRunner.java:65)
+	at org.apache.hadoop.util.ToolRunner.run(ToolRunner.java:79)
+	at org.apache.hadoop.fs.FsShell.main(FsShell.java:1895)
+real 76.91
+user 11.47
+sys 3.27
+Warning: $HADOOP_HOME is deprecated.
+
+java.lang.NullPointerException
+	at dm.util.MethodUtil.setMethod(Unknown Source)
+	at dm.util.MethodUtil.<init>(Unknown Source)
+	at dm.transformers.Transformers.transformClassForCodeSnippets(Unknown Source)
+	at dm.HDFSTransformer.transformClass(Unknown Source)
+	at dm.Transformer.transformClass(Unknown Source)
+	at dm.Transformer.transform(Unknown Source)
+	at sun.instrument.TransformerManager.transform(TransformerManager.java:188)
+	at sun.instrument.InstrumentationImpl.transform(InstrumentationImpl.java:424)
+	at com.sun.security.auth.module.UnixLoginModule.login(UnixLoginModule.java:124)
+	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
+	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.lang.reflect.Method.invoke(Method.java:606)
+	at javax.security.auth.login.LoginContext.invoke(LoginContext.java:762)
+	at javax.security.auth.login.LoginContext.access$000(LoginContext.java:203)
+	at javax.security.auth.login.LoginContext$4.run(LoginContext.java:690)
+	at javax.security.auth.login.LoginContext$4.run(LoginContext.java:688)
+	at java.security.AccessController.doPrivileged(Native Method)
+	at javax.security.auth.login.LoginContext.invokePriv(LoginContext.java:687)
+	at javax.security.auth.login.LoginContext.login(LoginContext.java:595)
+	at org.apache.hadoop.security.UserGroupInformation.getLoginUser(UserGroupInformation.java:414)
+	at org.apache.hadoop.security.UserGroupInformation.getCurrentUser(UserGroupInformation.java:395)
+	at org.apache.hadoop.fs.FileSystem$Cache$Key.<init>(FileSystem.java:1436)
+	at org.apache.hadoop.fs.FileSystem$Cache.get(FileSystem.java:1337)
+	at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:244)
+	at org.apache.hadoop.fs.FileSystem.get(FileSystem.java:122)
+	at org.apache.hadoop.fs.FsShell.init(FsShell.java:82)
+	at org.apache.hadoop.fs.FsShell.run(FsShell.java:1745)
+	at org.apache.hadoop.util.ToolRunner.run(ToolRunner.java:65)
+	at org.apache.hadoop.util.ToolRunner.run(ToolRunner.java:79)
+	at org.apache.hadoop.fs.FsShell.main(FsShell.java:1895)
+real 77.92
+user 12.36
+sys 4.18
+Warning: $HADOOP_HOME is deprecated.
+
+real 72.72
+user 3.06
+sys 1.53
+Warning: $HADOOP_HOME is deprecated.
+
+real 72.72
+user 2.80
+sys 1.48
